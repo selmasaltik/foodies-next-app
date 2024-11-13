49071ed4dbc310e9b88fe5a759f37c9a76b7e30b
selmasaltik <selmasaltiik@gmail.com>
2024-11-14 00:39:51 +0300

    Working with Server Action Responses & useActionState

--- 26-onwards-foodies-app/app/meals/share/page.js
+++ 26-onwards-foodies-app/app/meals/share/page.js
@@ -1,3 +1,7 @@
+'use client';
+
+import { useActionState } from 'react';
+
 import ImagePicker from '@/components/meals/image-picker';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { shareMeal } from '@/lib/actions';
@@ -5,6 +9,8 @@ import { shareMeal } from '@/lib/actions';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+    const [state, formAction] = useActionState(shareMeal, { message: null });
+
     return (
         <>
             <header className={classes.header}>
@@ -14,7 +20,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form} action={shareMeal}>
+                <form className={classes.form} action={formAction}>
                     <div className={classes.row}>
                     <p>
                         <label htmlFor="name">Your name</label>
@@ -43,6 +49,7 @@ export default function ShareMealPage() {
                         ></textarea>
                     </p>
                     <ImagePicker label="Your image" name="image" />
+                    {state.message && <p>{state.message}</p>}
                     <p className={classes.actions}>
                         <MealsFormSubmit />
                     </p>
